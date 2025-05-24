35f6bdac02fb198aabb36f13e260ae2323da955e
Yasa Kafi Razzan <114861219+YasaKafi@users.noreply.github.com>
2025-05-24 20:47:08 +0700

    feat : initial commit

---
+++ lib/utils/constant.dart
@@ -0,0 +1,28 @@
+String images = 'assets/image/';
+String svgImages = 'assets/svg/';
+String gif = 'assets/svg/';
+
+//CONSTANT API URL
+String baseUrl = '';
+
+//CONSTANT IMAGE
+String logoHashChat = '${images}logo_tokokita.png';
+
+//  CONSTANT GIF
+String logoHashChatGIF = '${gif}gif_tokokita.gif';
+
+
+//CONSTANT SVG
+String icHome = '${svgImages}ic_home.svg';
+String icStock = '${svgImages}ic_stock.svg';
+String icTransactions = '${svgImages}ic_transactions.svg';
+String icReports = '${svgImages}ic_reports.svg';
+String icHomeActive = '${svgImages}ic_home_active.svg';
+String icStockActive = '${svgImages}ic_stock_active.svg';
+String icTransactionsActive = '${svgImages}ic_transactions_active.svg';
+String icReportsActive = '${svgImages}ic_reports_active.svg';
+
+
+
+
+
